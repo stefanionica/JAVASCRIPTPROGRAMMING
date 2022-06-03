67f029db00e63d7f1e1efa9274c1fd72b72425b2
Stefan Ionica <stefan.ionica@histria.lan>
2022-06-03 17:44:09 +0300

    JAVASCRIPTPROGRAMMING

--- curs4/app1/module/Book.js
+++ curs4/app1/module/Book.js
@@ -1,13 +1,8 @@
 class Book {
-    constructor(title, author, isbn){
-      this.title = title;
-      this.author = author;
-      this.isbn = isbn;
-    }
+  constructor(title, author, isbn){
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
   }
-<<<<<<< HEAD
-  export {Book};
-=======
-
-  export{Book};
->>>>>>> 1948ebaf314dbc581b58152d54d6866f8c7cc57b
+}
+export {Book};
--- curs4/app1/module/Ui.js
+++ curs4/app1/module/Ui.js
@@ -1,60 +1,55 @@
 class UI {
-    addBookToList(book) {
-       const list = document.getElementById('book-list');
- 
-       // Create tr element
-       const row = document.createElement('tr');
-     
-       // Insert cols
-       row.innerHTML = `
-         <td>${book.title}</td>
-         <td>${book.author}</td>
-         <td>${book.isbn}</td>
-         <td><a href="#" class="delete">X</a></td>
-       `;
-     
-       list.appendChild(row);
-    }
- 
-    showAlert(message, className) {
-       // Create a div
-       const div = document.createElement('div');
-       // Add classes
-       div.className = `alert ${className}`;
-       // Add Text
-       div.appendChild(document.createTextNode(message));
-       // Get Parent
-       const container = document.querySelector('.container');
- 
-       // Put it before form
- 
-       // Get Form
-       const form = document.querySelector('#book-form');
-       // Insert Alert
-       container.insertBefore(div, form);
- 
-       // Timeout after 3 sec
-       setTimeout(function(){
-         document.querySelector('.alert').remove();
-       }, 3000);
-    }
- 
-    deleteBook(target) {
-     if(target.className === 'delete'){
-       target.parentElement.parentElement.remove();
-     }
-    }
- 
-    clearFields() {
-     document.getElementById('title').value = "";
-     document.getElementById('author').value = "";
-     document.getElementById('isbn').value = "";
-    }
- }
-<<<<<<< HEAD
- 
- export {UI}
-=======
+  addBookToList(book) {
+     const list = document.getElementById('book-list');
 
- export{UI};
->>>>>>> 1948ebaf314dbc581b58152d54d6866f8c7cc57b
+     // Create tr element
+     const row = document.createElement('tr');
+   
+     // Insert cols
+     row.innerHTML = `
+       <td>${book.title}</td>
+       <td>${book.author}</td>
+       <td>${book.isbn}</td>
+       <td><a href="#" class="delete">X</a></td>
+     `;
+   
+     list.appendChild(row);
+  }
+
+  showAlert(message, className) {
+     // Create a div
+     const div = document.createElement('div');
+     // Add classes
+     div.className = `alert ${className}`;
+     // Add Text
+     div.appendChild(document.createTextNode(message));
+     // Get Parent
+     const container = document.querySelector('.container');
+
+     // Put it before form
+
+     // Get Form
+     const form = document.querySelector('#book-form');
+     // Insert Alert
+     container.insertBefore(div, form);
+
+     // Timeout after 3 sec
+     setTimeout(function(){
+       document.querySelector('.alert').remove();
+     }, 3000);
+  }
+
+  deleteBook(target) {
+   if(target.className === 'delete'){
+     target.parentElement.parentElement.remove();
+   }
+  }
+
+  clearFields() {
+   document.getElementById('title').value = "";
+   document.getElementById('author').value = "";
+   document.getElementById('isbn').value = "";
+  }
+}
+
+export {UI}
